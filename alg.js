dbde69bee97bbcdca2e8b47b3226115ac8936e0e
Athkins Emmanuel Testimony <athkinstestimony@gmail.com>
2023-12-23 10:24:48 +0100

    Update alg.js total replacement

--- alg.js
+++ alg.js
@@ -1,5 +1,3 @@
-
-
 const btn = document.getElementById("btn")
 
 const validator = () => {
@@ -8,21 +6,22 @@ const validator = () => {
     let vowel = ["a", "e", "i", "o", "u"]
     let count = 0
     
+
     if (word.trim() == "") {
       alert("Please input a word")
       return
     }
-    
+
     if (word.trim().length > 30) {
       alert("Please input words and not sentences!")
       return
     }
-    
+
     let filter = word.split("")
     let newWord = filter.filter((item) => {
       return item !== " "
     })
-    
+
     for (let i = 0; i < newWord.length; i++) {
       if (!vowel.includes(newWord[i])) {
         count += 1
@@ -35,20 +34,41 @@ const validator = () => {
         return
       }
     }
-
     
+    
+
+
     if ("speechSynthesis" in window) {
       let message = new SpeechSynthesisUtterance()
       message.text = word
       message.voice = speechSynthesis.getVoices()[0]
-      message.rate = 0.5
-      message.pitch = 0.7
+      let ratespeed;
+      let radiospeed = document.querySelectorAll(".radiospeed")
+      radiospeed.forEach((item) => {
+          if(item.checked){
+              ratespeed = parseFloat(item.value)
+          }
+      })
+      console.log(typeof(ratespeed))
+      message.rate = ratespeed
+      
+      let rate;
+      let radio = document.querySelectorAll(".radio")
+      radio.forEach((item) => {
+          if(item.checked){
+              rate = parseFloat(item.value)
+          }
+      })
+      message.rate = ratespeed
+      message.pitch = rate
       speechSynthesis.speak(message)
-    } 
-    
+    }else{
+        alert("Speech Synthesis not supported in your browser, Please try again later!")
+    }
+
   } catch (error) {
     console.error("An error occurred:", error.message)
-    alert("Speech Synthesis not supported in your browser, Please try again")
+    alert("Speech Synthesis not supported in your browser, Please try again later!")
   }
 }
 
